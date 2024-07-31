8dc4d340cd846c3e65d3d8151d4b01635ed12f34
nicolas cristaldo <crist4545@gmail.com>
2024-07-30 23:19:53 -0300

    commit 1

---
+++ app/src/main/java/com/nicolascristaldo/superheroes/HeroesScreen.kt
@@ -0,0 +1,110 @@
+package com.nicolascristaldo.superheroes
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.nicolascristaldo.superheroes.model.Hero
+import com.nicolascristaldo.superheroes.model.HeroesRepository
+import com.nicolascristaldo.superheroes.ui.theme.SuperheroesTheme
+
+
+@Composable
+fun HeroItem(
+    hero: Hero,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp),
+        modifier = modifier.fillMaxWidth()
+    ) {
+        Row(
+            modifier = modifier
+                .padding(dimensionResource(id = R.dimen.padding_medium))
+                .sizeIn(minHeight = 72.dp)
+        ) {
+            HeroInformation(
+                name = hero.nameRes,
+                description = hero.descriptionRes,
+                modifier = modifier.weight(1f)
+            )
+            Spacer(modifier = modifier.width(16.dp))
+            HeroImage(image = hero.imageRes)
+        }
+    }
+}
+
+@Composable
+fun HeroInformation(
+    @StringRes name: Int,
+    @StringRes description: Int,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier
+    ) {
+        Text(
+            text = stringResource(id = name),
+            style = MaterialTheme.typography.displaySmall
+        )
+        Text(
+            text = stringResource(id = description),
+            style = MaterialTheme.typography.bodyLarge
+        )
+    }
+}
+
+@Composable
+fun HeroImage(
+    @DrawableRes image: Int,
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier = modifier
+            .size(dimensionResource(id = R.dimen.image_size))
+            .clip(MaterialTheme.shapes.small)
+    ) {
+        Image(
+            painter = painterResource(id = image),
+            contentDescription = null,
+            contentScale = ContentScale.Crop
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun Preview() {
+    SuperheroesTheme {
+        HeroItem(hero = HeroesRepository.heroes.last())
+    }
+}
---
+++ app/src/main/java/com/nicolascristaldo/superheroes/model/Hero.kt
@@ -0,0 +1,10 @@
+package com.nicolascristaldo.superheroes.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Hero(
+    @DrawableRes val imageRes: Int,
+    @StringRes val nameRes: Int,
+    @StringRes val descriptionRes: Int
+)
